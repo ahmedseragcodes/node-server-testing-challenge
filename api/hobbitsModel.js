7033d5bb74ea6a73961e41d9af72e42b33541cdc
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-22 20:38:06 -0400

    setup basic modeling and structure for router

--- api/hobbitsModel.js
+++ api/hobbitsModel.js
@@ -4,8 +4,28 @@ function getAllHobbits(){
     return db("hobbits");
 }
 
+function getHobbitById(id){
+    return db("hobbits")
+            .where("id", id)
+}
+
+async function addNewHobbit(newHobbit){
+    const newHobbitId = await db("hobbits")
+                            .insert(newHobbit)
+
+    return getHobbitById(newHobbitId);
+}
+
+function deleteHobbit(id){
+    return db("hobbits")
+            .where({id})
+            .del()
+
+}
+
+
 
 
 module.exports = {
-    
+    getAllHobbits, getHobbitById, addNewHobbit, deleteHobbit
 }
--- api/hobbitsRouter.js
+++ api/hobbitsRouter.js
@@ -7,7 +7,25 @@ const router = express.Router();
 
 //[GET] ALL HOBBITS
 
+router.get("/", (req, res, next)=>{
 
+})
+
+//[GET] BY ID
+router.get("/:id", (req, res, next)=>{
+
+})
+
+//[POST] New Hobbit
+router.post("/", (req, res, next)=>{
+
+})
+
+//[DELETE] Hobbit
+router.delete("/:id", (req, res, next)=>{
+
+    
+})
 
 
 module.exports = router;
